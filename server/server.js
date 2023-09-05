e615bdccdf49e34881c356cd2032dc60b6f0b69b
Axh <anikahussain1@outlook.com>
2023-09-05 17:56:02 +0100

    nvmrc added, func for authen routes fixed

--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+process.env.NODE_ENV = 'production';
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
